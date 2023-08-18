fe71fed05cab97549b45a34d51cb8944fd1aeaef
cc-greatman <blessedgreatman96@gmail.com>
2023-08-18 01:10:23 +0100

    FIX: Debugged form

--- index.php
+++ index.php
@@ -681,7 +681,7 @@
                     </div>
                     <div class="col-lg-6 offset-lg-1 valign">
                         <div class="full-width">
-                            <form method="post" action="/sendmail">
+                            <form method="post" action="/sendmail.php">
 
                                 <div class="messages"></div>
 
