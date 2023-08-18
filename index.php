66ee4af7a5c2a8c01195eab9f399e1e2df7700ec
cc-greatman <blessedgreatman96@gmail.com>
2023-08-18 01:05:25 +0100

    FIX: Debugged .htaccess

--- index.php
+++ index.php
@@ -681,7 +681,7 @@
                     </div>
                     <div class="col-lg-6 offset-lg-1 valign">
                         <div class="full-width">
-                            <form method="post" action="https://bitsys.ng/sendmail">
+                            <form method="post" action="/sendmail">
 
                                 <div class="messages"></div>
 
